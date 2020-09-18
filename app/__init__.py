d68ad508b367c208456c12048a9c51d4484b0bbf
ErikBoesen <me@erikboesen.com>
2020-09-18 16:32:29 -0600

    Add __init__

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from config import Config
+from celery import Celery
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cas import CAS
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+cors = CORS(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+cas = CAS(app)
+
+from app import routes, errors
