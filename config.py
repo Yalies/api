bac2dc6f94c8780ea117f53e1a463f93b7c13477
ErikBoesen <me@erikboesen.com>
2020-09-18 17:55:24 -0600

    Remove SECURITY_PASSWORD_SALT from config

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Override this in production')
-    SECURITY_PASSWORD_SALT = os.environ.get('SECURITY_PASSWORD_SALT', 'Override this too')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
                                              'sqlite:///' + os.path.join(basedir, 'app.db'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
