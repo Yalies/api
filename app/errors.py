17077940b2a20dfa0e20c23b3db6d0c6fab180b8
ErikBoesen <me@erikboesen.com>
2020-09-18 16:32:48 -0600

    Add error pages

---
+++ app/errors.py
@@ -0,0 +1,18 @@
+from flask import render_template
+from app import app, db
+
+
+@app.errorhandler(401)
+def not_found_error(error):
+    return render_template('401.html'), 401
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
