4c0b70450ec0f394a6626eed906150782f29ee05
ErikBoesen <me@erikboesen.com>
2020-09-18 18:50:44 -0600

    Add from_token method on User

--- app/models.py
+++ app/models.py
@@ -27,6 +27,26 @@ class User(db.Model):
             algorithm='HS256'
         ).decode(), payload['exp']
 
+    @staticmethod
+    def from_token(token):
+        """
+        Decode/validate an auth token.
+        :param token: token to decode.
+        :return: User whose token this is, or None if token invalid/no user associated
+        """
+        try:
+            payload = jwt.decode(token, app.config.get('SECRET_KEY'))
+            """
+            is_blacklisted = BlacklistedToken.check_blacklist(token)
+            if is_blacklisted:
+                # Token was blacklisted following logout
+                return None
+            """
+            return User.query.get(payload['sub'])
+        except (jwt.ExpiredSignatureError, jwt.InvalidTokenError):
+            # Signature expired, or token otherwise invalid
+            return None
+
 
 class Student(db.Model):
     __tablename__ = 'students'
