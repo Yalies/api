02ea5e72ccf2b7bd8a8798a60c10fabb84e94e10
ErikBoesen <me@erikboesen.com>
2020-09-18 18:47:38 -0600

    Actually display token

--- app/static/js/credentials.js
+++ app/static/js/credentials.js
@@ -15,7 +15,8 @@ reset_token.onclick = function(e) {
     req.send();
     req.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            location.reload();
+            var response = JSON.parse(this.responseText);
+            readout_token.value = response.token;
         }
     };
 };
