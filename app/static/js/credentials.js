f4c86b84958410d8e1fd43fe9d3fb7f768de2c92
ErikBoesen <me@erikboesen.com>
2020-09-18 18:34:40 -0600

    Add credentials JS

---
+++ app/static/js/credentials.js
@@ -0,0 +1,25 @@
+const readout_token = document.getElementById("readout_token"),
+      copy_token = document.getElementById("copy_token"),
+      reset_token = document.getElementById("reset_token");
+
+
+copy_token.onclick = function(e) {
+    readout_token.select();
+    document.execCommand('copy');
+}
+
+reset_token.onclick = function(e) {
+    var req = new XMLHttpRequest();
+    req.open("POST", "/credentials");
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.send();
+    req.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            location.reload();
+        }
+    };
+};
+
+readout_token.onfocus = function(e) {
+    this.select();
+}
