6b48bac509b995ed8b371fd0c0bd70249cf832eb
ErikBoesen <me@erikboesen.com>
2020-09-20 14:39:10 -0600

    Add initial s3 upload code

---
+++ app/s3.py
@@ -0,0 +1,24 @@
+import boto3, botocore
+
+S3_BUCKET_NAME = 'yalestudentphotos'
+S3_ACCESS_KEY = os.environ.get('S3_ACCESS_KEY')
+S3_SECRET_ACCESS_KEY = os.environ.get('S3_SECRET_ACCESS_KEY')
+S3_LOCATION = 'https://' + S3_BUCKET + '.s3.amazonaws.com/'
+
+s3 = boto3.client(
+   's3',
+   aws_access_key_id=S3_ACCESS_KEY,
+   aws_secret_access_key=S3_SECRET_ACCESS_KEY,
+)
+
+def upload_image(image_id, file):
+    s3.upload_fileobj(
+        file,
+        S3_BUCKET_NAME,
+        f'{image_id}.jpg',
+        ExtraArgs={
+            'ACL': 'public-read',
+            'ContentType': file.content_type
+        }
+    )
+    return '{}{}'.format(app.config['S3_LOCATION'], file.filename)
