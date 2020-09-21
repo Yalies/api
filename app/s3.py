5245267121a034544b76da2e4fef308088ec9e1b
ErikBoesen <me@erikboesen.com>
2020-09-21 09:20:34 -0600

    Create ImageUploader class

--- app/s3.py
+++ app/s3.py
@@ -7,30 +7,30 @@ S3_ACCESS_KEY = os.environ.get('S3_ACCESS_KEY')
 S3_SECRET_ACCESS_KEY = os.environ.get('S3_SECRET_ACCESS_KEY')
 S3_LOCATION = 'https://' + S3_BUCKET_NAME + '.s3.amazonaws.com/'
 
-s3 = boto3.client(
-   's3',
-   aws_access_key_id=S3_ACCESS_KEY,
-   aws_secret_access_key=S3_SECRET_ACCESS_KEY,
-)
+class ImageUploader:
+    def __init__(self):
+        self.s3 = boto3.client(
+           's3',
+           aws_access_key_id=S3_ACCESS_KEY,
+           aws_secret_access_key=S3_SECRET_ACCESS_KEY,
+        )
 
+    def get_image_ids():
+        objs = self.s3.list_objects(Bucket=S3_BUCKET_NAME)
+        image_ids = {int(obj['Key'].rstrip('.jpg')) for obj in objs['Contents']}
+        return image_ids
 
-def upload_image(image_id, f):
-    filename = f'{image_id}.jpg'
-    print('Uploading image %s with size %d bytes.' % (filename, f.getbuffer().nbytes))
-    f.seek(0)
-    s3.upload_fileobj(
-        f,
-        S3_BUCKET_NAME,
-        filename,
-        ExtraArgs={
-            'ACL': 'public-read',
-            'ContentType': 'image/jpeg',
-        }
-    )
-    return '{}{}'.format(S3_LOCATION, filename)
-
-
-def get_image_ids():
-    objs = s3.list_objects(Bucket=S3_BUCKET_NAME)
-    image_ids = {int(obj['Key'].rstrip('.jpg')) for obj in objs['Contents']}
-    return image_ids
+    def upload_image(image_id, f):
+        filename = f'{image_id}.jpg'
+        print('Uploading image %s with size %d bytes.' % (filename, f.getbuffer().nbytes))
+        f.seek(0)
+        self.s3.upload_fileobj(
+            f,
+            S3_BUCKET_NAME,
+            filename,
+            ExtraArgs={
+                'ACL': 'public-read',
+                'ContentType': 'image/jpeg',
+            }
+        )
+        return '{}{}'.format(S3_LOCATION, filename)
