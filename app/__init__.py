e77081fca82c6da01263d5b8ec24f376743742bd
ErikBoesen <me@erikboesen.com>
2020-09-26 21:43:24 -0600

    Remove whooshee import

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from config import Config
 from celery import Celery
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from flask_whooshee import Whooshee
 from flask_migrate import Migrate
 from flask_cas import CAS
 from elasticsearch import Elasticsearch
