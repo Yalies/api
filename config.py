3f33f585d072a417c55ff16b100700105b38bae9
ErikBoesen <me@erikboesen.com>
2020-09-26 22:09:35 -0600

    Add ELASTICSEARCH_URL to config

--- config.py
+++ config.py
@@ -12,6 +12,8 @@ class Config(object):
 
     CELERY_BROKER_URL = CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
 
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+
     CAS_SERVER = 'https://secure.its.yale.edu'
     CAS_AFTER_LOGIN = 'index'
     CAS_LOGIN_ROUTE = '/cas/login'
