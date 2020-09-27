ed0fe70013c5de2af126711f47c4b3c90fa9ccba
ErikBoesen <me@erikboesen.com>
2020-09-27 01:24:02 -0600

    Reindent ES query

--- app/search.py
+++ app/search.py
@@ -21,8 +21,18 @@ def query_index(index, query, page, per_page):
         return [], 0
     search = elasticsearch.search(
         index=index,
-        body={'query': {'multi_match': {'query': query, 'operator': 'AND', 'fields': ['*']}},
-              'from': (page - 1) * per_page, 'size': per_page})
+        body={
+            'query': {
+                'multi_match': {
+                    'query': query,
+                    'operator': 'AND',
+                    'fuzziness': 'AUTO',
+                    'fields': ['*']
+                }
+            },
+            'from': (page - 1) * per_page,
+            'size': per_page
+        })
     ids = [int(hit['_id']) for hit in search['hits']['hits']]
     return ids, search['hits']['total']['value']
 
