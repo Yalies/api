de1e07353c0a19f8b2b1430f4f559cce53067980
ErikBoesen <me@erikboesen.com>
2020-09-26 21:10:42 -0600

    Include elasticsearch

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_whooshee import Whooshee
 from flask_migrate import Migrate
 from flask_cas import CAS
+from elasticsearch import Elasticsearch
 
 
 app = Flask(__name__)
@@ -14,10 +15,10 @@ celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 cors = CORS(app)
 db = SQLAlchemy(app)
-whooshee = Whooshee(app)
-whooshee.reindex()
 migrate = Migrate(app, db)
 cas = CAS(app)
+elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
+    if app.config['ELASTICSEARCH_URL'] else None
 
 from app import routes, errors, api
 app.register_blueprint(api.api_bp, url_prefix='/api')
