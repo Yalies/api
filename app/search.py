1860fe3673cb2fab38d54f11331145796ba78d6f
ErikBoesen <me@erikboesen.com>
2020-09-27 01:16:20 -0600

    Capitalize operator

--- app/search.py
+++ app/search.py
@@ -21,7 +21,7 @@ def query_index(index, query, page, per_page):
         return [], 0
     search = elasticsearch.search(
         index=index,
-        body={'query': {'multi_match': {'query': query, 'operator': 'and', 'fields': ['*']}},
+        body={'query': {'multi_match': {'query': query, 'operator': 'AND', 'fields': ['*']}},
               'from': (page - 1) * per_page, 'size': per_page})
     ids = [int(hit['_id']) for hit in search['hits']['hits']]
     return ids, search['hits']['total']['value']
