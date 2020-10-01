f0c182624c43732c0e0bb6d486da391f02037db1
ErikBoesen <me@erikboesen.com>
2020-09-30 21:40:11 -0600

    Improve spacing on popup

--- app/static/js/popup.js
+++ app/static/js/popup.js
@@ -4,11 +4,11 @@ function showPopup() {
     let content = document.createElement('div');
     content.className = 'content';
     let header = document.createElement('h1');
-    header.textContent = 'Warning';
+    header.innerHTML = '<i class="fa fa-exclamation-triangle"></i> Warning';
     content.appendChild(header);
     paragraphs = [
         'All data used on Yalies is already publicly accessible on the <a href="https://students.yale.edu/facebook">Yale Face Book</a> and the <a href="https://directory.yale.edu">Yale Directory</a>. Certain information provided by Yale is understandably disconcerting to some students, including street addresses, room numbers, and occasionally phone numbers.',
-        'Yalies censors such information, however, we can\'t do anything about what\'s on Yale\'s official sources. Though Yale ought to ultimately make this sensitive data hidden by default, for now we encourage students to visit the <a href="https://students.yale.edu/facebook/PreferencesPage">Face Book preferences page</a> and remove information that makes them uncomfortable. Extensive documentation on how to remove your data from Yale\'s sources can be found <a href="/hide_me">here</a>.',
+        'Yalies censors such information, however, we can\'t do anything about what\'s on Yale\'s official sources. Though Yale ought to ultimately make this sensitive data hidden by default, for now we encourage students to visit the <a href="https://students.yale.edu/facebook/PreferencesPage">Yale Face Book preferences page</a> and remove information that makes them uncomfortable. Further documentation on how to remove your data from Yale\'s sources can be found <a href="/hide_me">here</a>.',
     ];
     for (let paragraph of paragraphs) {
         let p = document.createElement('p');
