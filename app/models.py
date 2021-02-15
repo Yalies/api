f220f0f3da40ec09de8269d034f5d34a7a53da52
ErikBoesen <me@erikboesen.com>
2021-02-15 17:11:53 -0500

    Reorder properties of person

--- app/models.py
+++ app/models.py
@@ -56,9 +56,14 @@ class Person(SearchableMixin, db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
+    # Identifiers
     netid = db.Column(db.String)
     upi = db.Column(db.Integer)
+    email = db.Column(db.String)
+    mailbox = db.Column(db.String)
+    phone = db.Column(db.String)
 
+    # Naming
     title = db.Column(db.String)
     first_name = db.Column(db.String, nullable=False)
     nickname = db.Column(db.String)
@@ -67,41 +72,37 @@ class Person(SearchableMixin, db.Model):
     suffix = db.Column(db.String)
     pronoun = db.Column(db.String)
 
+    # Students
+    school_code = db.Column(db.String)
+    school = db.Column(db.String)
+    year = db.Column(db.Integer)
+    curriculum = db.Column(db.String)
+    # Undergrads
+    college = db.Column(db.String)
+    college_code = db.Column(db.String)
+    leave = db.Column(db.Boolean)
+    eli_whitney = db.Column(db.Boolean)
     image_id = db.Column(db.Integer)
     image = db.Column(db.String)
-    year = db.Column(db.Integer)
-    college = db.Column(db.String)
-    email = db.Column(db.String)
+    birthday = db.Column(db.String)
     residence = db.Column(db.String)
     building_code = db.Column(db.String)
     entryway = db.Column(db.String)
     floor = db.Column(db.Integer)
     suite = db.Column(db.Integer)
     room = db.Column(db.String)
-    birthday = db.Column(db.String)
     major = db.Column(db.String)
     address = db.Column(db.String)
-    phone = db.Column(db.String)
     access_code = db.Column(db.String)
-    leave = db.Column(db.Boolean, default=False)
-    eli_whitney = db.Column(db.Boolean, default=False)
 
-    # Staff fields
+    # Staff
     organization_id = db.Column(db.String)
     organization = db.Column(db.String)
     unit_class = db.Column(db.String)
     unit_code = db.Column(db.String)
     unit = db.Column(db.String)
-
-    # Student fields
-    school_code = db.Column(db.String)
-    school = db.Column(db.String)
-    college_code = db.Column(db.String)
-    curriculum = db.Column(db.String)
-    office = db.Column(db.String)
-    mailbox = db.Column(db.String)
     postal_address = db.Column(db.String)
-
+    office = db.Column(db.String)
 
     @staticmethod
     def search(criteria):
