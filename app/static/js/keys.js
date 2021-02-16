b457a173c049f6dcd627c0b25dd03e04c9940191
ErikBoesen <me@erikboesen.com>
2021-02-15 21:37:13 -0500

    Complete keys table initialization

---
+++ app/static/js/keys.js
@@ -0,0 +1,63 @@
+const readout_key = document.getElementById('readout_key'),
+      get_key = document.getElementById('get_key'),
+      keys_list = document.getElementById('keys_list');
+
+
+get_key.onclick = function(e) {
+    fetch('/keys', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+        .then(response => response.json())
+        .then(json => {
+            readout_key.value = json.key;
+            readout_key.select();
+            document.execCommand('copy');
+            get_key.textContent = 'Copied!';
+            setTimeout(function() {
+                get_key.textContent = 'Get key';
+            }, 1500);
+        });
+};
+
+readout_key.onfocus = function(e) {
+    this.select();
+}
+
+
+function delete_key(id) {
+
+}
+
+
+fetch('/keys', {
+    method: 'GET',
+})
+    .then(response => response.json())
+    .then(json => {
+        for (let key of json) {
+            let tr = document.createElement('tr');
+            for (let property of ['key', 'description']) {
+                let td = document.createElement('td');
+                td.textContent = key[property];
+                tr.appendChild(td);
+            }
+            // Create delete button
+            let td = document.createElement('td');
+            let button = document.createElement('button');
+            button.className = 'delete';
+            let icon = document.createElement('i');
+            icon.className = 'fa fa-trash';
+            button.appendChild(icon);
+            button.appendChild(document.createTextNode('Delete'));
+            button.onclick = function() {
+                delete_key(key.id);
+            };
+            td.appendChild(button);
+            tr.appendChild(td);
+
+            keys_list.appendChild(tr);
+        }
+    });
