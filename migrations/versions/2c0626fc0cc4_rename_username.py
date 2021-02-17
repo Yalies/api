7244eb85947a8dbced9096bacca64c77fa6f786d
ErikBoesen <me@erikboesen.com>
2021-02-16 20:28:42 -0500

    Alter column before creating new foreign key

--- migrations/versions/2c0626fc0cc4_rename_username.py
+++ migrations/versions/2c0626fc0cc4_rename_username.py
@@ -19,11 +19,11 @@ depends_on = None
 def upgrade():
     op.add_column('key', sa.Column('user_id', sa.String(), nullable=True))
     op.drop_constraint('key_user_username_fkey', 'key', type_='foreignkey')
+    op.alter_column('users', 'username', new_column_name='id')
     op.create_foreign_key('key_user_id_fkey', 'key', 'users', ['user_id'], ['id'])
     op.drop_column('key', 'user_username')
     #op.add_column('users', sa.Column('id', sa.String(), nullable=False))
     #op.drop_column('users', 'username')
-    op.alter_column('users', 'username', new_column_name='id')
 
 
 def downgrade():
