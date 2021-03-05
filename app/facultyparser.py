ed537b03880e201e8269fb81187248edc8ffc12c
ErikBoesen <me@erikboesen.com>
2021-03-05 18:43:28 -0500

    Add faculty parser script

---
+++ app/facultyparser.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+
+"""
+html = requests.get('https://www.yale.edu/academics/departments-programs').text
+soup = BeautifulSoup(html, 'html.parser')
+links = soup.find_all('a', {'class': 'department_item_link'})
+departments = [
+    {
+        'name': link.text,
+        'url': link['href'],
+    } for link in links
+]
+print(json.dumps(departments))
+"""
+
+
