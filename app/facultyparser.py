cfcba19dec67be933e8e93f15ac57b2d49abc9d4
ErikBoesen <me@erikboesen.com>
2021-03-05 20:20:48 -0500

    Clean up parsed content from person page

--- app/facultyparser.py
+++ app/facultyparser.py
@@ -24,7 +24,7 @@ people = []
 def extract_field(parent, field_name):
     elem = parent.find('div', {'class': 'field-name-field-' + field_name})
     if elem is not None:
-        return elem.text
+        return elem.text.strip().replace('\xa0', ' ')
 
 
 for department in departments:
@@ -35,20 +35,28 @@ for department in departments:
         people_page = requests.get(department['url'] + path).text
         people_soup = BeautifulSoup(people_page, 'html.parser')
 
-        cards = soup.find_all('div', {'class': 'views-row'})
+        cards = people_soup.find_all('div', {'class': 'views-row'})
         for card in cards:
             person = {
                 'profile_url': department['url'] + card.find('a', {'class': 'username'})['href']
             }
-            person_page = requests.get(person['profile_url'])
+            person_page = requests.get(person['profile_url']).text
             person_soup = BeautifulSoup(person_page, 'html.parser')
 
-            body = person_soup.find('div', {'id': 'section-content'})
+            body = person_soup.find('main', {'id': 'section-content'})
             name = body.find('h1', {'class': 'title'})
             person.update({
-                'name': name.text,
+                'name': name.text.strip(),
                 'status': extract_field(body, 'status'),
+                'email': extract_field(body, 'email'),
                 'education': extract_field(body, 'education'),
-                'bio': extract_field(body, 'bio'),
+                'bio': None,
             })
+            bio = extract_field(body, 'bio')
+            if bio is not None:
+                person['bio'] = bio.lstrip('_').lstrip()
 
+            print('Parsed ' + person['name'])
+            people.append(person)
+
+print(people)
