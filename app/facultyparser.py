c598c1292e18aa085ed57b7df535a369cd81abaf
ErikBoesen <me@erikboesen.com>
2021-03-06 16:10:19 -0500

    Parse architecture pages

--- app/facultyparser.py
+++ app/facultyparser.py
@@ -241,10 +241,27 @@ def parse_path_architecture(path, department):
         links_page = people_page_soup.select('div.faculty-member-thumbnail a')
         if len(links_page) == 0:
             break
+        print(f'Found {len(links_page)} people on page {page}.')
         profile_urls_page = [department['url'] + link['href'] for link in links_page]
         profile_urls += profile_urls_page
         page += 1
-    print(profile_urls)
+
+    for profile_url in profile_urls:
+        person = {
+            'profile_url': profile_url,
+        }
+        person_soup = get_soup(profile_url)
+        image = person_soup.select_one('.faculty-show__top-area img')
+        if image is not None:
+            image = image['srcset'].split('?')[0].replace('/convert', '')
+            person['image'] = image
+        person['name'] = person_soup.find('h1', {'class': 'h2'}).text
+        title = person_soup.find('div', {'class': 'h2'})
+        if title:
+            person['title'] = title.text
+        # TODO: parse bio and education as well
+        print('Parsing ' + person['name'])
+        people.append(person)
     return people
 
 
