74f20a9d03edcd262a5697f21d4e56924aea5b92
ErikBoesen <me@erikboesen.com>
2021-03-05 20:11:02 -0500

    Add iteration through people

--- app/facultyparser.py
+++ app/facultyparser.py
@@ -16,4 +16,39 @@ departments = [
 print(json.dumps(departments))
 """
 
+with open('res/departments.json', 'r') as f:
+    departments = json.load(f)
+
+people = []
+
+for department in departments:
+    if department.get('paths') is None:
+        continue
+
+    for path in department['paths']:
+        people_page = requests.get(department['url'] + path).text
+        people_soup = BeautifulSoup(people_page, 'html.parser')
+
+        cards = soup.find_all('div', {'class': 'views-row'})
+        for card in cards:
+            person = {
+                'profile_url': department['url'] + card.find('a', {'class': 'username'})['href']
+            }
+            person_page = requests.get(person['profile_url'])
+            person_soup = BeautifulSoup(person_page, 'html.parser')
+
+            body = person_soup.find('div', {'id': 'section-content'})
+            name = body.find('h1', {'class': 'title'})
+            person['name'] = name.text
+
+            status = body.find('div', {'class': 'field-name-field-status'})
+            if status is not None:
+                person['status'] = status.text
+            education = body.find('div', {'class': 'field-name-field-education'})
+            if education is not None:
+                person['education'] = education.text
+            bio = body.find('div', {'class': 'field-name-field-bio'})
+            if bio is not None:
+                person['bio'] = bio.text
+
 
