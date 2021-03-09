e634e72b4c5c66edf28bcecebc932cd81beedea0
ErikBoesen <me@erikboesen.com>
2021-03-08 21:50:05 -0500

    Fix architecture adapter

--- app/scraper/adapters/architecture.py
+++ app/scraper/adapters/architecture.py
@@ -3,13 +3,13 @@ from .adapter import Adapter
 
 class Architecture(Adapter):
 
-    def scrape_path(department, path):
+    def scrape_path(self, department, path):
         people = []
 
         page = 1
         profile_urls = []
         while True:
-            people_page_soup = get_soup(department['url'] + path, params={'page': page})
+            people_page_soup = self.get_soup(department['url'] + path, params={'page': page})
             links_page = people_page_soup.select('div.faculty-member-thumbnail a')
             if len(links_page) == 0:
                 break
@@ -21,7 +21,7 @@ class Architecture(Adapter):
             person = {
                 'profile_url': profile_url,
             }
-            person_soup = get_soup(profile_url)
+            person_soup = self.get_soup(profile_url)
             image = person_soup.select_one('.faculty-show__top-area img')
             if image is not None:
                 image = image['srcset'].split('?')[0].replace('/convert', '')
