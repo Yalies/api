695e5cd3be45715a8cef140b831621585b85356e
ErikBoesen <me@erikboesen.com>
2021-03-09 21:28:49 -0500

    Finish parsing nursing website

--- app/scraper/adapters/nursing.py
+++ app/scraper/adapters/nursing.py
@@ -19,6 +19,7 @@ class Nursing(Adapter):
             }
             person_soup = self.get_soup(profile_url)
             person['name'], person['suffix'] = self.split_name_suffix(person_soup.find('h1', {'id': 'page-title'}).text)
+            person['name'] = self.NICKNAME_RE.sub('', person['name'])
             banner = person_soup.find('div', {'class': 'row-1-banner'})
             image = banner.select_one('div.field-name-field-photo img')
             if image and 'facultyblank.jpg' not in image['src']:
@@ -27,20 +28,25 @@ class Nursing(Adapter):
             contact_container = banner.select_one('.field-name-field-person-contact-information .field-item')
             if contact_container:
                 contact_elems = contact_container.find_all('p', recursive=False)
-                if len(contact_elems):
-                    person['title'] = contact_elems.pop(0).text.strip()
-                if len(contact_elems):
-                    strong = contact_elems[0].find('strong')
-                    print(strong)
-                    if strong is None or not strong.text.strip():
-                        person['room_number'] = contact_elems.pop(0).text.strip()
-                for contact_elem in contact_elems:
-                    # phone, email, fax list. Usually one element, sometimes in multiple
-                    # Multiple example: https://nursing.yale.edu/faculty-research/faculty-directory/samantha-conley-phd-rn-fnp-bc
-                    contacts = contact_elems.pop(0).text.strip()
+                # First, pop contact data lists at bottom
+                # Usually they're in a single element, but sometimes two, such as:
+                # https://nursing.yale.edu/faculty-research/faculty-directory/samantha-conley-phd-rn-fnp-bc
+                while len(contact_elems):
+                    if ':' not in contact_elems[-1].text:
+                        break
+                    contacts = contact_elems.pop().text.strip()
+                    print(contacts)
                     for contact in contacts.split('\n'):
                         label, value = contact.split(':')
                         person[label] = value.strip()
+
+                if len(contact_elems):
+                    strong = contact_elems[-1].find('strong')
+                    if not strong or not strong.text.strip():
+                        person['room_number'] = contact_elems.pop().text.strip()
+
+                if len(contact_elems):
+                    person['title'] = '; '.join([elem.text.strip() for elem in contact_elems])
 
             people.append(person)
             print('Parsed ' + person['name'])
