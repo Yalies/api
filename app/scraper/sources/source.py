3ea945bd58121a057e54d9aa3a7b06be22d312ec
ErikBoesen <me@erikboesen.com>
2021-03-15 00:27:11 -0400

    Complete source.py base class

---
+++ app/scraper/sources/source.py
@@ -0,0 +1,45 @@
+import re
+
+class Source:
+    ###########
+    # Utilities
+    ###########
+
+    RE_PHONE_COUNTRY_CODE = re.compile(r'^\+1? ')
+    RE_PHONE_DISALLOWED_CHARACTERS = re.compile(r'[A-Za-z\(\) \-\.]')
+
+    def clean_phone(phone):
+        if not phone:
+            return phone
+        if type(phone) == int:
+            phone = str(phone)
+        phone = self.RE_PHONE_COUNTRY_CODE.sub('', phone)
+        phone = self.RE_PHONE_DISALLOWED_CHARACTERS.sub('', phone)
+        return phone
+
+    ###############
+    # Scraping flow
+    ###############
+
+    def scrape(self):
+        """
+        Read all people from this source.
+        """
+        raise NotImplementedError
+
+    def merge(self, current_people, new_people):
+        """
+        Given list of people from previous sources, merge in newly scraped people.
+        :param current_people: list of people scraped from previous sources.
+        :param new_people: list of new people scraped from this source.
+        """
+        raise NotImplementedError
+
+    def integrate(self, current_people):
+        """
+        Run scraper and integrate results into existing list of people.
+        :param people: list of existing people from previous sources.
+        :return: list with new records integrated.
+        """
+        new_people = self.scrape()
+        return self.merge(current_people, new_people)
