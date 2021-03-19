9e901435c9a3b37626465b95f5b8dfa686c8f22e
ErikBoesen <me@erikboesen.com>
2021-03-19 19:14:21 -0400

    Fix postgres URL

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
 
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Override this in production')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
-                                             'sqlite:///' + os.path.join(basedir, 'app.db'))
+                                             'sqlite:///' + os.path.join(basedir, 'app.db')).replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     CELERY_BROKER_URL = CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
