299487b80763ae258176870815963e11854cc461
ErikBoesen <me@erikboesen.com>
2023-12-05 13:35:00 -0500

    Add actually using error 403 page

--- app/errors.py
+++ app/errors.py
@@ -3,16 +3,21 @@ from app import app, db
 
 
 @app.errorhandler(401)
-def not_found_error(error):
+def error_401(error):
     return render_template('401.html'), 401
 
 
+@app.errorhandler(403)
+def error_403(error):
+    return render_template('403.html'), 403
+
+
 @app.errorhandler(404)
-def not_found_error(error):
+def error_404(error):
     return render_template('404.html'), 404
 
 
 @app.errorhandler(500)
-def internal_error(error):
+def error_500(error):
     db.session.rollback()
     return render_template('500.html'), 500
